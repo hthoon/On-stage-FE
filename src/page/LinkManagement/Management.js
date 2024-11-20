819267ec944f9c94278c25822efe68ddb6c42eaa
HVLOSHIN <halogod9985@naver.com>
2024-11-20 18:45:27 +0900

    LinkManagement Layout, UI

--- src/page/LinkManagement/Management.js
+++ src/page/LinkManagement/Management.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import {useLink} from "../../context/LinkContext";
+import React, {useState} from 'react';
 import LinkDisplay from "../../components/LinkDisplay/LinkDisplay";
-import "./Management.css"; // 스타일 파일 추가
+import "./Management.css";
+import ManagementPannel from "./ManagementPannel";
 
 const Management = () => {
-    //const { link, linkDetail, socialLink, theme } = useLink();
 
 
     return (
         <div className="management-container">
-            {/* 링크 관리 패널 */}
-            <div className="management-panel">
-                <button>새 링크 추가</button>
-                <button>링크 정렬</button>
-                <button>설정</button>
-            </div>
-
+            <ManagementPannel/>
 
             {/* 디스플레이 패널 */}
             <div className="display-panel">
-            <LinkDisplay/>
+                <LinkDisplay/>
             </div>
         </div>
     )
