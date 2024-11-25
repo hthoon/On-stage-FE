a752758f324016c05f0e91a0ba9b4a3ef9af379a
HVLOSHIN <halogod9985@naver.com>
2024-11-25 16:18:57 +0900

    Theme layout css 세부 설정

--- src/page/LinkManagement/ThemeSwitcher.js
+++ src/page/LinkManagement/ThemeSwitcher.js
@@ -5,6 +5,7 @@ import { useLink } from "../../context/LinkContext";
 const ThemeSwitcher = () => {
     const { theme, updateTheme } = useLink();
     const [customTheme, setCustomTheme] = useState(theme || {});
+    const [isOpen, setIsOpen] = useState(false); // 토글 상태 관리
 
     useEffect(() => {
         // CSS 변수 업데이트
@@ -24,55 +25,63 @@ const ThemeSwitcher = () => {
     };
 
     return (
-        <div className="theme-switcher">
-            <h3>Customize Theme</h3>
-            <div className="theme-setting">
-                <label>
-                    버튼 색상:
-                    <input
-                        type="color"
-                        value={customTheme.buttonColor || "#ffffff"}
-                        onChange={(e) =>
-                            handleThemeChange("buttonColor", e.target.value)
-                        }
-                    />
-                </label>
-            </div>
-            <div className="theme-setting">
-                <label>
-                    폰트 색상:
-                    <input
-                        type="color"
-                        value={customTheme.fontColor || "#000000"}
-                        onChange={(e) =>
-                            handleThemeChange("fontColor", e.target.value)
-                        }
-                    />
-                </label>
-            </div>
-            <div className="theme-setting">
-                <label>
-                    아이콘 색상:
-                    <input
-                        type="color"
-                        value={customTheme.iconColor || "#000000"}
-                        onChange={(e) =>
-                            handleThemeChange("iconColor", e.target.value)
-                        }
-                    />
-                </label>
-            </div>
-            <div className="theme-setting">
-                <label>
-                    프로필 색상:
-                    <input
-                        type="color"
-                        value={customTheme.profileColor || "#000000"}
-                        onChange={(e) =>
-                            handleThemeChange("profileColor", e.target.value)
-                        }
-                    />
-                </label>
+        <div className="themeSwitcher-container">
+            <button
+                className="theme-toggle-button"
+                onClick={() => setIsOpen(!isOpen)} // 토글 상태 변경
+            >
+                {isOpen ? "테마 닫기" : "테마 열기"}
+            </button>
+            <div className={`theme-switcher ${isOpen ? "open" : "close"}`}>
+                <h3>테마</h3>
+                <div className="theme-setting">
+                    <label>
+                        버튼 색상:
+                        <input
+                            type="color"
+                            value={customTheme.buttonColor || "#ffffff"}
+                            onChange={(e) =>
+                                handleThemeChange("buttonColor", e.target.value)
+                            }
+                        />
+                    </label>
+                </div>
+                <div className="theme-setting">
+                    <label>
+                        폰트 색상:
+                        <input
+                            type="color"
+                            value={customTheme.fontColor || "#000000"}
+                            onChange={(e) =>
+                                handleThemeChange("fontColor", e.target.value)
+                            }
+                        />
+                    </label>
+                </div>
+                <div className="theme-setting">
+                    <label>
+                        아이콘 색상:
+                        <input
+                            type="color"
+                            value={customTheme.iconColor || "#000000"}
+                            onChange={(e) =>
+                                handleThemeChange("iconColor", e.target.value)
+                            }
+                        />
+                    </label>
+                </div>
+                <div className="theme-setting">
+                    <label>
+                        프로필 색상:
+                        <input
+                            type="color"
+                            value={customTheme.profileColor || "#000000"}
+                            onChange={(e) =>
+                                handleThemeChange("profileColor", e.target.value)
+                            }
+                        />
+                    </label>
+                </div>
             </div>
         </div>
     );
