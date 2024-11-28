31a0c0057d87016f9677be70a98d25e9402092b4
Sur0n <skyzephyr333@gmail.com>
2024-11-28 11:32:34 +0900

    feat : 지도 컴포넌트 그리기 테스트

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Management from "../page/LinkManagement/Management";
+//TODO map 테스트
+import KakaoMap from '../components/MapDisplay/MapDisplay';
 
 function AppRouter() {
     return (
 
         <Routes>
             <Route path="/management" element={<Management />} />
+            <Route path="/map" element={<KakaoMap />} />
         </Routes>
+          
     )
 
 }
