a066385342170f401e5723b22f012784503082e6
HVLOSHIN <halogod9985@naver.com>
2024-12-02 10:18:22 +0900

    공유 URL 구현

---
+++ src/page/LinkManagement/VisitPage.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from "react";
+import LinkDisplay from "../../components/LinkDisplay/LinkDisplay";
+
+const VisitPage = () => {
+    return (
+        <>
+            <div className="LinkDisplay">
+                <LinkDisplay />
+            </div>
+        </>
+    );
+};
+export default VisitPage;
