8ad12daa6f270f19aa5f2c38ec06b3f580a95c1a
thoon <winwin_th@naver.com>
2024-12-03 16:58:44 +0900

    feat: 테스트용 마이페이지

--- src/router/Router.js
+++ src/router/Router.js
@@ -4,8 +4,9 @@ import Management from "../page/LinkManagement/Management";
 //TODO map 테스트
 import KakaoMap from '../components/MapDisplay/MapDisplay';
 import Home from "../page/home/Home";
-import Login from "../page/home/Login";
+import Login from "../page/login/Login";
 import VisitPage from "../page/LinkManagement/VisitPage";
+import MyPage from "../page/mypage/MyPage";
 
 function AppRouter() {
     return (
@@ -16,6 +17,7 @@ function AppRouter() {
             <Route path="/page/:username" element={<VisitPage />} />
             <Route path="/map" element={<KakaoMap />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/mypage" element={<MyPage />} />
         </Routes>
           
     )
