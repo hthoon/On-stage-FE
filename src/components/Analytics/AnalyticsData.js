98bf97bb2f76c1884ee29107e99e13de5d6d2fb4
HVLOSHIN <halogod9985@naver.com>
2024-12-12 11:44:00 +0900

    Analytics UX 개선 및 이전 기간 과 비교 기능 구현

--- src/components/Analytics/AnalyticsData.js
+++ src/components/Analytics/AnalyticsData.js
@@ -1,18 +1,62 @@
 import React from 'react';
 import { TrendingUp, Link2, Eye } from 'lucide-react';
 import './css/AnalyticsData.css';
+import {TbTriangleFilled, TbTriangleInvertedFilled} from "react-icons/tb";
 
-const AnalyticsData = ({ analyticsData = {} }) => {
+const AnalyticsData = ({ analyticsData, prevAnalyticsData }) => {
     // 안전한 데이터 처리
-    const pageViewStats = analyticsData.pageViewStats || [];
+    const currentPageViewStats = analyticsData.pageViewStats || [];
 
     // 페이지 조회수, 링크 클릭 수, 클릭률 합산 계산
-    const totalPageViewCount = pageViewStats.reduce((total, data) => total + (data.pageViewCount || 0), 0);
-    const totalLinkClickCount = pageViewStats.reduce((total, data) => total + (data.linkClickCount || 0), 0);
+    const totalPageViewCount = currentPageViewStats.reduce((total, data) => total + (data.pageViewCount || 0), 0);
+    const totalLinkClickCount = currentPageViewStats.reduce((total, data) => total + (data.linkClickCount || 0), 0);
     const totalClickRate =
         totalPageViewCount > 0
             ? ((totalLinkClickCount / totalPageViewCount) * 100).toFixed(2)
             : 0;
+
+    const prevPageViewStats = prevAnalyticsData.pageViewStats || [];
+    const prevTotalPageViewCount = prevPageViewStats.reduce((total, data) => total + (data.pageViewCount || 0), 0);
+    const prevTotalLinkClickCount = prevPageViewStats.reduce((total, data) => total + (data.linkClickCount || 0), 0);
+
+    const prevTotalClickRate =
+        prevTotalPageViewCount > 0
+            ? ((prevTotalLinkClickCount / prevTotalPageViewCount) * 100).toFixed(2)
+            : 0;
+
+
+
+    const renderGapTrend = (gap) => {
+        if (gap > 0)
+            return (
+                <p className="Analytics-data-up"><TbTriangleFilled />{gap}</p>
+
+            );
+        if (gap < 0)
+            return (
+                <p className="Analytics-data-down"><TbTriangleInvertedFilled />{gap}</p>
+
+            );
+        return (
+            <p className="Analytics-data-down">-</p>
+        );
+    };
+
+    const renderGapTrendRate = (gap) => {
+        if (gap > 0)
+            return (
+                <p className="Analytics-data-up"><TbTriangleFilled />{gap.toFixed(2)}</p>
+
+            );
+        if (gap < 0)
+            return (
+                <p className="Analytics-data-down"><TbTriangleInvertedFilled />{gap.toFixed(2)}</p>
+
+            );
+        return (
+            <p className="Analytics-data-down">-</p>
+        );
+    };
 
     return (
         <div className="analytics-data-container">
@@ -29,26 +73,36 @@ const AnalyticsData = ({ analyticsData = {} }) => {
                 <div className="analytics-statistics-container">
                     <div className="statistic-card total-page-views-card">
                         <div className="statistic-content">
-                            <Eye className="statistic-icon" />
+                            <Eye className="statistic-icon"/>
                             <span className="statistic-label">방문</span>
                         </div>
                         <span className="statistic-value">{totalPageViewCount}</span>
+                        <div>
+                            {renderGapTrend(totalPageViewCount - prevTotalPageViewCount)}
+                        </div>
+
                     </div>
 
                     <div className="statistic-card total-link-clicks-card">
                         <div className="statistic-content">
-                            <Link2 className="statistic-icon" />
+                            <Link2 className="statistic-icon"/>
                             <span className="statistic-label">클릭</span>
                         </div>
                         <span className="statistic-value">{totalLinkClickCount}</span>
+                        <div>
+                            {renderGapTrend(totalLinkClickCount - prevTotalLinkClickCount)}
+                        </div>
                     </div>
 
                     <div className="statistic-card total-click-rate-card">
                         <div className="statistic-content">
-                            <TrendingUp className="statistic-icon" />
+                            <TrendingUp className="statistic-icon"/>
                             <span className="statistic-label">클릭 비율</span>
                         </div>
                         <span className="statistic-value">{totalClickRate}%</span>
+                        <div>
+                            {renderGapTrendRate(totalClickRate - prevTotalClickRate)}
+                        </div>
                     </div>
                 </div>
             )}
