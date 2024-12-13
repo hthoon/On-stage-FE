b3b0f6eb72715f4571933dec9ad3b84e174b983b
thoon <131009655+hthoon@users.noreply.github.com>
2024-12-13 14:31:41 +0900

    feat: 문자 인증

--- src/page/mypage/MyPage.js
+++ src/page/mypage/MyPage.js
@@ -4,8 +4,45 @@ import SocialPanel from "../LinkManagement/SocialPanel";
 import {IoSettingsSharp} from "react-icons/io5";
 import {MdVerified} from "react-icons/md";
 import {AiFillDollarCircle} from "react-icons/ai";
+import {useAxios} from "../../context/AxiosContext";
 function MyPage() {
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [verifyCode, setVerifyCode] = useState(false);
+    const [isCodeSent, setIsCodeSent] = useState(false);
+    const [verificationCode, setVerificationCode] = useState('');
+    const { axiosInstance } = useAxios();
 
+    const sendCode = async(e) => {
+        e.preventDefault();
+        try {
+            const response = await axiosInstance.post(`/api/user/send`, {
+                phoneNumber,
+            });
+            if (response.status === 200) {
+                setIsCodeSent(true);
+                alert('인증번호가 발송되었습니다!');
+            }
+        } catch (err) {
+            console.error("저장 중 오류 발생:", err);
+        }
+    }
+
+    const checkCode = async(e) => {
+        e.preventDefault();
+        try {
+            const response = await axiosInstance.post(`/api/user/verify`, {
+                phoneNumber,
+                verificationCode,
+            });
+            if (response.status === 200) {
+                setVerifyCode(true);
+                alert('인증이 완료되었습니다!');
+                window.location.reload();
+            }
+        } catch (err) {
+            console.error("저장 중 오류 발생:", err);
+        }
+    }
 
     return (
         <div className="mypage-wrapper">
@@ -20,17 +57,51 @@ function MyPage() {
                 <form className="badge-application-form">
                         <div>
                             <label htmlFor="tel">휴대폰 번호:</label>
-                            <input type="tel" id="tel" name="tel" placeholder="'-' 없이 입력하세요" required/>
+                            <input
+                                type="tel"
+                                id="tel"
+                                name="tel"
+                                placeholder="'-' 없이 입력하세요"
+                                required
+                                value={phoneNumber} // 입력 필드의 값
+                                onChange={(e) => setPhoneNumber(e.target.value)} // 상태 업데이트
+                            />
                         </div>
-                        <div>
-                            <label htmlFor="reason">신청 사유</label>
-                            <textarea id="reason" name="reason" placeholder="인증 배지를 신청하는 이유를 입력하세요" required></textarea>
-                        </div>
-                        <div>
-                            <button type="submit">신청하기</button>
-                        </div>
-                    </form>
+                    <div>
+                        <button onClick={sendCode}>신청하기</button>
+                    </div>
+                </form>
                 </div>
+
+                {isCodeSent && (
+                    <div className="mypage-divider">
+                        <h2>인증번호 입력</h2>
+                        <form className="badge-application-form"
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                                console.log("인증번호:", verificationCode);
+                            }}
+                        >
+                            <div>
+                                <label htmlFor="verification-code">인증번호:</label>
+                                <input
+                                    type="text"
+                                    id="verification-code"
+                                    name="verification-code"
+                                    placeholder="인증번호를 입력하세요"
+                                    required
+                                    value={verificationCode}
+                                    onChange={(e) =>
+                                        setVerificationCode(e.target.value)
+                                    }
+                                />
+                            </div>
+                            <div>
+                                <button onClick={checkCode}>인증 확인</button>
+                            </div>
+                        </form>
+                    </div>
+                )}
 
 
                 <h1><AiFillDollarCircle className="mypage-text-icon" />요금제 안내</h1>
